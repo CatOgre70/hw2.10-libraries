1ee615c6d944d58c70d9d20a9dd7815c47a1771f
Vasily Demin <vasily.demin@gmail.com>
2022-06-28 11:38:22 +0300

    Initial commit

---
+++ src/test/java/sky/pro/hw210libraries/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.hw210libraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
